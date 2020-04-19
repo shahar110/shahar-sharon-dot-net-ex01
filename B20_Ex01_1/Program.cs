f6c66d72c5397b7e32fee592807eb3fd9296a147
sedri14 <47485395+sedri14@users.noreply.github.com>
2020-04-19 13:58:46 +0300

    fixing ex01_1 to be without classes

--- B20_Ex01_1/Program.cs
+++ B20_Ex01_1/Program.cs
@@ -9,9 +9,12 @@ namespace B20_Ex01_1
             string userInput1 = "";
             string userInput2 = "";
             string userInput3 = "";
+            int m_decimalInput1;
+            int m_decimalInput2;
+            int m_decimalInput3;
 
             System.Console.WriteLine("Please enter 3 binary numbers, 9 digits each: ");
-      
+                  
             for (int i = 0; i < 3; i++)
             {
                 userInput = System.Console.ReadLine();
@@ -35,23 +38,25 @@ namespace B20_Ex01_1
                 }
             }
 
-            NumberStats stats = new NumberStats(userInput1, userInput2, userInput3);
+            m_decimalInput1 = binaryStringToInt(userInput1);
+            m_decimalInput2 = binaryStringToInt(userInput2);
+            m_decimalInput3 = binaryStringToInt(userInput3);
 
             System.Console.WriteLine(string.Format(
 @"The decimal numbers are: {0} {1} {2}.
-The average number of 0's is: {3}, the average number of 1's is: {4}.
+The average number of 0's is: {3:0.0}, the average number of 1's is: {4:0.0}.
 There are {5} numbers which are power of two.
 There are {6} numbers which are in ascending order.
 The smallest number is: {7}, the biggest number is: {8}.",
-               stats.GetFirstDecimal(),
-               stats.GetSecondDecimal(),
-               stats.GetThirdDecimal(),
-               stats.CalculateAverageDigit('0'),
-               stats.CalculateAverageDigit('1'),
-               stats.AmountOfPowerOf2(),
-               stats.AmountOfAscendingOrderNumbers(),
-               stats.GetMin(),
-               stats.GetMax()
+               m_decimalInput1,
+               m_decimalInput2,
+               m_decimalInput3,
+               CalculateAverageDigit(userInput1, userInput2, userInput3, '0'),
+               CalculateAverageDigit(userInput1, userInput2, userInput3, '1'),
+               AmountOfPowerOf2(m_decimalInput1, m_decimalInput2, m_decimalInput3),
+               AmountOfAscendingOrderNumbers(m_decimalInput1, m_decimalInput2, m_decimalInput3),
+               GetMin(m_decimalInput1, m_decimalInput2, m_decimalInput3),
+               GetMax(m_decimalInput1, m_decimalInput2, m_decimalInput3)
                ));
 
             System.Console.ReadLine();
@@ -79,6 +84,136 @@ The smallest number is: {7}, the biggest number is: {8}.",
             }
 
             return isValid;
+        }
+        
+        private static int binaryStringToInt(string binaryNumber)
+        {
+            int decimalNumber = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                int power = 9 - 1 - i;
+                decimalNumber += (binaryNumber[i] - '0') * (int)System.Math.Pow(2, power);
+            }
+
+            return decimalNumber;
+        }
+
+        private static int GetMin(int i_FirstNumber, int i_SecondNumber, int i_ThirdNumber)
+        {
+            return System.Math.Min(System.Math.Min(i_FirstNumber, i_SecondNumber), i_ThirdNumber);
+        }
+
+        private static int GetMax(int i_FirstNumber, int i_SecondNumber, int i_ThirdNumber)
+        {
+            return System.Math.Max(System.Math.Max(i_FirstNumber, i_SecondNumber), i_ThirdNumber);
+        }
+
+        private static int AmountOfPowerOf2(int i_FirstNumber, int i_SecondNumber, int i_ThirdNumber)
+        {
+            int amountOfPowerOf2 = 0;
+
+            if (isPowerOf2(i_FirstNumber))
+            {
+                amountOfPowerOf2++;
+            }
+
+            if (isPowerOf2(i_SecondNumber))
+            {
+                amountOfPowerOf2++;
+            }
+
+            if (isPowerOf2(i_ThirdNumber))
+            {
+                amountOfPowerOf2++;
+            }
+
+            return amountOfPowerOf2;
+        }
+
+        private static bool isPowerOf2(int i_DecimalNumber)
+        {
+            bool isPowerOfTwo = false;
+
+            if (i_DecimalNumber == 0)
+            {
+                isPowerOfTwo = false;
+            }
+            if (System.Math.Ceiling(System.Math.Log(i_DecimalNumber, 2)) == System.Math.Floor(System.Math.Log(i_DecimalNumber, 2)))
+            {
+                isPowerOfTwo = true;
+            }
+
+            return isPowerOfTwo;
+        }
+
+        private static int AmountOfAscendingOrderNumbers(int i_FirstNumber, int i_SecondNumber, int i_ThirdNumber)
+        {
+            int amountOfAscending = 0;
+
+            if (isAscendingOrder(i_FirstNumber))
+            {
+                amountOfAscending++;
+            }
+
+            if (isAscendingOrder(i_SecondNumber))
+            {
+                amountOfAscending++;
+            }
+
+            if (isAscendingOrder(i_ThirdNumber))
+            {
+                amountOfAscending++;
+            }
+
+            return amountOfAscending;
+        }
+
+        private static bool isAscendingOrder(int i_DecimalNumber)
+        {
+            bool m_isAscendingSeries = true;
+            int currentReadDigit = i_DecimalNumber % 10;
+            i_DecimalNumber /= 10;
+            int nextDigitToCheck = i_DecimalNumber % 10;
+
+            while (i_DecimalNumber != 0)
+            {
+                if (currentReadDigit > nextDigitToCheck)
+                {
+                    currentReadDigit = nextDigitToCheck;
+                    i_DecimalNumber /= 10;
+                    nextDigitToCheck = i_DecimalNumber % 10;
+                }
+                else
+                {
+                    m_isAscendingSeries = false;
+                    break;
+                }
+            }
+
+            return m_isAscendingSeries;
+        }
+
+        private static double CalculateAverageDigit(string userInput1, string userInput2, string userInput3, char i_Digit)
+        {
+            return (countDigitInString(userInput1, i_Digit)
+                   + countDigitInString(userInput2, i_Digit)
+                   + countDigitInString(userInput3, i_Digit)) / 3.0;
+        }
+
+        private static int countDigitInString(string i_StringNumber, char i_Digit)
+        {
+            int numberOfDigits = 0;
+
+            for (int i = 0; i < i_StringNumber.Length; i++)
+            {
+                if (i_StringNumber[i].Equals(i_Digit))
+                {
+                    numberOfDigits++;
+                }
+            }
+
+            return numberOfDigits;
         }
     }
 }
